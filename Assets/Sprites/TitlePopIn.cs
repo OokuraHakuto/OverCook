764617561b96b0b0c133a29d156f821fe7fccbb3
ToaKogawa <t.624a.124@softbank.ne.jp>
2025-07-25 11:04:02 +0900

    7/25
    タイトル表示にアニメーションが付きました

---
+++ Assets/Sprites/TitlePopIn.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening; 
+
+public class TitlePopIn : MonoBehaviour
+{
+    public Image titleImage; 
+
+    void Start()
+    {
+        
+        transform.localScale = Vector3.zero;
+        titleImage.color = new Color(1, 1, 1, 0);
+
+        //ポップインアニメーション
+        transform.DOScale(Vector3.one, 0.5f).SetEase(Ease.OutBack);
+
+        // フェードイン
+        titleImage.DOFade(1f, 0.5f);
+    }
+}
