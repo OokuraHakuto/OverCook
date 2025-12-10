161de74581ca6bca31e44603de711a6f40488a4b
G24013 <hm-g24013@sist.ac.jp>
2025-12-10 12:23:20 +0900

    '25/12/10
    時間経過ゲージのプレハブをGameMain_UIフォルダに作成。使用する際はCanvas_3D内に入れること。
    ２パターンつくってみたのでお好みの方をどうぞ(黄ゲージ上昇後、新たに赤ゲージが上昇/黄ゲージ上昇後、色がだんだん赤くなる)

---
+++ Assets/GameMain_UI/Spricts/GaugeManager2.cs
@@ -0,0 +1,62 @@
+using DG.Tweening;
+using System;
+using UniGLTF.Extensions.VRMC_vrm;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugeManager2 : MonoBehaviour
+{
+    public GameObject sliderObj;
+    public Slider slider1;
+    public Image fillArea;
+    public float time;
+    public Transform camera;
+
+    Color col;
+
+    void Start()
+    {
+        time = 0;
+        slider1.value = 0;
+
+        col = new Color(1, 1, 0);
+        fillArea.color = col;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time <= 5)
+        {
+            slider1.value = time;
+        }
+        else if (time <= 10)
+        {
+            col.g = 1 * ((10-time) / 5);
+            fillArea.color = col;
+        }
+        else
+        {
+            if (time % 0.5f <= 0.25f)
+            {
+                col.r -= 0.02f;
+                col.g -= 0.02f;
+                fillArea.color = col;
+            }
+            else
+            {
+                col.r += 0.02f;
+                col.g += 0.02f;
+                fillArea.color = col;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.R)) Start();
+
+        /*
+        slider1Obj.transform.LookAt(camera);
+        slider2Obj.transform.LookAt(camera);
+        */
+    }
+}
